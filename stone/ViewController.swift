58443b67aaf99ea9aca19becf13964fd394e5d8e
xty19 <915484383@qq.com>
2016-07-23 13:39:01 +0800

    1
    1

---
+++ stone/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  stone
+//
+//  Created by xty19 on 16/4/30.
+//  Copyright © 2016年 xty19. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
